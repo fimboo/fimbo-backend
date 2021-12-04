891a2768a16421f1e1fea8a3928df675fef99e1c
dusabeyezu florentine <dusaflora2@gmail.com>
2021-12-04 21:00:55 +0200

    change config

--- src/resources/api/v1/business/business.routes.js
+++ src/resources/api/v1/business/business.routes.js
@@ -2,11 +2,11 @@ import express from "express"
 import businessController from './business.controller'
 import CheckAuthorisation from "../../../../middlewares/authentication"
 import checkowner from "../../../../middlewares/checkOwnership"
-import BusinessValidation from "./BusinessValidation"
+
 
 const router = express.Router()
 
-router.post("/create",CheckAuthorisation,BusinessValidation.details,businessController.createBusiness)
+router.post("/create",CheckAuthorisation,businessController.createBusiness)
 router.get("/all",CheckAuthorisation,checkowner.mine,businessController.allBusiness)
 router.get("/one/:id",CheckAuthorisation,checkowner.mine,businessController.myBusiness)
 router.put("/edit/:id",CheckAuthorisation,checkowner.mine,businessController.editBusiness)
